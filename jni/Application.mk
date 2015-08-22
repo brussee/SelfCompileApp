74855b42139b23fe4c7d690f6f04e447c1f99a88
Intel 478 <intel_478@hotmail.com>
2015-08-22 23:52:08 +0200

    update libpng & expat; copy java-ide-droid;

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,6 @@
 NDK_TOOLCHAIN_VERSION := 4.9
 APP_OPTIM    := debug #release
 APP_STL      := gnustl_static #stlport_static #c++_static #gabi++_static
-APP_MODULES  := libcutils libutils zipalign #expat libpng libhost #aidl #aapt
-APP_ABI      := all
-APP_PLATFORM := android-21
+#APP_MODULES  := libcutils libutils zipalign #expat libpng libhost #aidl #aapt
+APP_ABI      := armeabi #armeabi-v7a x86 #x86_64 mips mips64 arm64-v8a
+APP_PLATFORM := android-18
